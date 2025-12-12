c36e7cf1e6123f6c91ac150481fec829733c7ed4
itz-rudr <rudrag26.2@gmail.com>
2025-12-12 14:26:11 +0530

    To explain parametrized constructor

---
+++ CPP/parrameterized_constuctor_explain.cpp
@@ -0,0 +1,42 @@
+//To explain parametrized constructor
+#include<iostream>
+using namespace std;
+class complex{
+	private: int a,b;
+	//This also show that constructor can be overloaded
+	public: complex(){
+				a=0;
+				b=0;
+			}
+			complex(int x){
+				a=x;
+				b=0;
+			}
+			complex(int x,int y){
+				a=x;
+				b=y;
+			}
+			void out(){
+				cout<<a<<" + i"<<b<<endl;
+			}
+};
+int main(){
+	//Implicit constructor calling
+	complex p;
+	complex q(2);
+	complex t(3,2);
+	p.out();
+	q.out();
+	t.out();
+}
+/*
+Explicit constructor calling
+int main(){
+complex p=complex();
+complex q=complex(2);
+complex t=complex(3,2);
+p.out();
+q.out();
+t.out();
+}
+*/
