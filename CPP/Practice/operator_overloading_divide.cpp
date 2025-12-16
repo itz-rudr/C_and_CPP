8c34a937ce19d28079e6075535a3bf236f80d900
itz-rudr <rudrag26.2@gmail.com>
2025-12-16 19:53:39 +0530

    To overload a operator in / operator

---
+++ CPP/Practice/operator_overloading_divide.cpp
@@ -0,0 +1,37 @@
+//To explain how to overload a function 
+//Using / operator
+#include<iostream>
+using namespace std;
+class complex{
+	private: int a,b;
+	public: complex(int x=0,int y=0){
+		a=x;
+		b=y;
+	}
+	void get(){
+		cout<<"Enter real and imagenery value"<<endl;
+		cin>>a>>b;
+	}
+	void out(){
+		cout<<a<<" + i"<<b<<endl;
+	}
+	complex operator +(complex x){
+		complex w;
+		w.a=a+x.a;
+		w.b=b+x.b;
+		return w;
+	}
+	complex operator /(complex x){
+		complex w;
+		w.a=(a*x.a+b*x.b)/(x.a*x.a+x.b*x.b);
+		w.b=(b*x.a-a*x.b)/(x.a*x.a+x.b*x.b);
+		return w;
+	}
+};
+int main(){
+	complex p,q,t;
+	p.get();
+	q.get();
+	t=p/q;
+	t.out();
+}
