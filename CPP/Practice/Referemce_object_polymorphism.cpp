c5a1e5cff2a1bf069f7ab312eb04b5e698c17cf3
itz-rudr <rudrag26.2@gmail.com>
2025-12-20 18:34:09 +0530

    To explain how to use polymorphism using reference variable

---
+++ CPP/Practice/Referemce_object_polymorphism.cpp
@@ -0,0 +1,19 @@
+//To explain polymorphism using reference object
+#include<iostream>
+using namespace std;
+class test{
+	private: int a,b;
+	public: void get(){
+		cout<<"Enter two no.s: "<<endl;
+		cin>>a>>b;
+	}
+	void out(){
+		cout<<a<<endl<<b<<endl;
+	}
+};
+int main(){
+	test t;
+	test &u=t;
+	u.get();
+	u.out();
+}
