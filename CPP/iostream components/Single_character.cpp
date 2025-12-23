946a17e58bff7f5b7f3dc48de9c632eaf82c8d7f
itz-rudr <rudrag26.2@gmail.com>
2025-12-23 23:13:05 +0530

    To explain how to inout and output single character using iostream components

---
+++ CPP/iostream components/Single_character.cpp
@@ -0,0 +1,9 @@
+//To eplain how to input and output single character using iostream components
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter character = "<<endl;
+	ch=cin.get();
+	cout.put(ch);
+}
