2b3fafb9b853415fa8b5f1471d5ecc29ca0c65ad
itz-rudr <rudrag26.2@gmail.com>
2025-12-24 23:07:52 +0530

    To explain file handling (Creation)

---
+++ CPP/File Handling/file_handling_creation.cpp
@@ -0,0 +1,34 @@
+//To explain file handling in cpp
+/*
+File handling--> This is used to permanently store data in a file
+it performs various file operations such as
+file open, file write, file read, file close.
+syntaxes -->
+to open a file --> class_name object("file name");
+				   ofstream f("abc.dat") extension does not matter
+				   for output file stream
+				   ifstream f("abc.dat")
+				   for input stream
+*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+class student{
+	private: char name[10];
+			 int roll;
+	public: void get(){
+		cout<<"Enter name and roll"<<endl;
+		cin>>name>>roll;
+	}
+	void out(){
+		cout<<"Name = "<<name<<endl;
+		cout<<"Roll = "<<roll<<endl;
+	}
+};
+int main(){
+	student p;
+	ofstream f("abc.dat");
+	p.get();
+	f.write((char*)(&p),sizeof(p));
+	f.close();
+}
