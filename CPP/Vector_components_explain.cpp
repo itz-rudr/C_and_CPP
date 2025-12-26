2e8d20ea30121090dcd5d3c875829f08cd89471c
itz-rudr <rudrag26.2@gmail.com>
2025-12-26 22:12:23 +0530

    To explain components in a vector file

---
+++ CPP/Vector_components_explain.cpp
@@ -0,0 +1,21 @@
+//To explain iterator in c++
+//To explain componenets of a vector file
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int>v{1,2,3,4,5};
+	vector<int>::iterator it;//declare iterator
+	//Initialize the iterator with the first element
+	it=v.begin();
+	//print the vector element
+	cout<<"v[0] = "<<*it<<endl;
+	//iterator points to the 3rd element
+	it=v.begin()+2;
+	cout<<"v[2] = "<<*it<<endl;
+	//iterator points to the 3rd element
+	it=v.begin()-1;
+	//iterator points to the last element
+	it=v.end()-1;
+	cout<<"v[4] = "<<*it<<endl;
+}
